305fa99a58980e0b2f21569722ac2fd2f93accbe
Anusthan Singh <102304867+anusthan12@users.noreply.github.com>
2026-09-25 17:37:11 +0200

    Update NgoGrid.tsx

--- app/components/ngo/NgoGrid.tsx
+++ app/components/ngo/NgoGrid.tsx
@@ -1,14 +1,18 @@
 import NgoCard from "./NgoCard";
 import { ngoWork, NgoProps } from "./ngoDetails";
+import AnimatedTitle from "../../animations/AnimatedTitle";
 import React from "react";
 
 const NgoGrid = () => {
     return (
         <>
             <div className="mb-8 md:mb-10 lg:mb-12 px-4">
-                <h4 className="text-[16px] md:text-[20px] lg:text-[28px] text-[#e4ded7]">
-                    NGO & Volunteer Work
-                </h4>
+                <AnimatedTitle
+                    text="NGO & Volunteer Work"
+                    className="mt-10 mb-4 text-left text-[32px] font-bold tracking-tighter text-[#e4ded7] sm:text-[36px] md:mb-6 md:text-[48px] lg:text-[64px]"
+                    wordSpace="mr-[10px]"
+                    charSpace="mr-[0.001em]"
+                />
             </div>
 
             <div className="grid w-[95%] mx-auto grid-cols-1 gap-6 sm:gap-6 md:grid-cols-2 lg:grid-cols-2 lg:gap-8 lg:max-w-[1200px]">
