d9ab010e861fc143453bbd9d37064e49678c45fa
Anusthan Singh <102304867+anusthan12@users.noreply.github.com>
2026-09-25 17:33:41 +0200

    Update Ngo.tsx

--- app/sections/Ngo.tsx
+++ app/sections/Ngo.tsx
@@ -7,10 +7,6 @@ const Ngo = () => {
             className="relative z-10 flex w-full flex-col items-center justify-center bg-[#0E1016] bg-cover bg-center py-16 md:py-20 lg:py-20"
             id="ngo"
         >
-            <h2 className="mb-10 hidden text-[36px] text-[#e4ded7] md:mb-16 md:text-[42px] lg:mb-16 lg:text-[72px]">
-                NGO & Volunteer Work
-            </h2>
-
             <NgoGrid />
         </section>
     );
